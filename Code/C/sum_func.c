5846313080338162a7f51e8a95ba8bc2f051539e
agamtomar <agamtomar@engineering.ucla.edu>
2018-07-30 15:00:39 -0700

    Sum Function in C

---
+++ Code/C/sum_func.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+int sumInt(int a, int b);
+
+int main() {
+	int int1, int2, sum;
+	
+	printf("Enter first integer: ");
+	scanf("%d", &int1);
+	
+	printf("Enter second integer: ");
+	scanf("%d", &int2);
+	
+	sum = sumInt(int1, int2);
+	
+	printf("The sum of integers %d + %d is %d", int1, int2, sum);
+	
+	return (0);
+	}
+
+int sumInt(int a, int b){
+	int c;
+	c = a + b;
+	return (c);
+	}
